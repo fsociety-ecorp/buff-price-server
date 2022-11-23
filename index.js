8d891532a78be73d9b8afa22db4671129632c6d5
Ricardo Cardoso <riques.fcardoso@gmail.com>
2022-11-23 23:21:36 +0000

    updated redirect routes

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 import buffRoutes from './routes/buff.js';
+import connectDatabase from './config/mongodb.js';
 
 const app = express();
 const PORT = 5000;
+const API_PREFIX = '/api'
 
 app.use(bodyParser.json());
 
 app.use(cors({ origin: '*' }));
 
-app.get('/', (req, res) => res.send('Welcome to BUFF163/CSGORoll Pricing Server'));
+app.use(`${API_PREFIX}/buff`, buffRoutes);
 
-app.use('/buff', buffRoutes);
+// if user is not logged-in redirect back to login page
+app.use(function (req, res) {
+    res.redirect(301, '/');
+});
+
+connectDatabase();
 
 app.listen(process.env.PORT || PORT, () => console.log(`Server running on: https://localhost:${PORT}`));
